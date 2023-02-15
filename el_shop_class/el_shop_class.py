e7c713560237781f084c54f8b9ffe4a36e13fa4e
DSulzhits <dsulzhits@gmail.com>
2023-02-15 16:36:41 +0300

    Создание класса

---
+++ el_shop_class/el_shop_class.py
@@ -0,0 +1,26 @@
+class Item:
+    pay_rate = 1
+    item_list = []
+
+    def __init__(self, name, price=0, quantity=0):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.item_list.append(self)
+
+    def full_price(self):
+        return self.price
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+        return self.price
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+    # def __repr__(self):
+    #     return f"Товар: {self.name}, цена {self.price}, скидка {self.pay_rate}, количество {self.quantity}"
+
+
+
+
