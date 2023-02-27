bcf042e888fe8b939a83ec5603fc4108d2c05e08
DSulzhits <dsulzhits@gmail.com>
2023-02-27 20:25:00 +0300

    Добавлена фикстура

--- test_class/conftest.py
+++ test_class/conftest.py
@@ -1,5 +1,6 @@
 import pytest
-
+from el_shop_class.el_shop_class import Item
+"""Необходимые для тестов фикстуры"""
 
 @pytest.fixture
 def test_class():
@@ -7,3 +8,11 @@ def test_class():
     price = 10000
     quantity = 20
     return name, price, quantity
+
+@pytest.fixture
+def instantiate_from_csv():
+    filename = 'test.csv'
+    Item.instantiate_from_csv(filename)
+    item1 = Item.item_list[3]
+    return Item.instantiate_from_csv(filename), item1
+
