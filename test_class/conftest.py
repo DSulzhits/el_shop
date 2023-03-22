916f31e7a97ee90985a918303da38fc79c13155f
DSulzhits <dsulzhits@gmail.com>
2023-03-22 20:35:42 +0300

    Добавлены фикстуры

--- test_class/conftest.py
+++ test_class/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from el_shop_class.el_shop_class import Item
-from el_shop_class.items_class import Phone
+from el_shop_class.items_class import Phone, Keyboard
 
 """Необходимые для тестов фикстуры"""
 
@@ -9,9 +9,15 @@ from el_shop_class.items_class import Phone
 def class_test():
     return Item("Смартфон", 10000, 20)
 
+
 @pytest.fixture
 def phone_test():
     return Phone("Смартфон", 10000, 20, 2)
+
+
+@pytest.fixture
+def keyboard_test():
+    return Keyboard('Dark Project KD87A', 9600, 5)
 
 
 @pytest.fixture
