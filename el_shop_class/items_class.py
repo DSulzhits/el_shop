7ebaf107e875e8fa7871a17237da921750dd251e
DSulzhits <dsulzhits@gmail.com>
2023-03-22 20:36:45 +0300

    Добавлены классы KBLanguage, Keyboard и их методы, также убран __repr__ для класса Phone

--- el_shop_class/items_class.py
+++ el_shop_class/items_class.py
@@ -21,5 +21,22 @@ class Phone(Item):
         else:
             raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
 
-    def __repr__(self):
-        return f"Phone({self.name}, {self.price}, {self.quantity}, {self.number_of_sim})"
+
+class KBLanguage:
+    __language = "EN"
+
+    def change_language(self):
+        if self.__language == "EN":
+            self.__language = "RU"
+        else:
+            self.__language = "EN"
+
+    @property
+    def language(self):
+        return self.__language
+
+
+class Keyboard(Item, KBLanguage):
+    def __init__(self, name: str, price: float, quantity: int, language="EN"):
+        super().__init__(name, price, quantity)
+        self.__language = language
